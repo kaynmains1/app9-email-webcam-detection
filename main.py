bc28b2dd41bcf74b61efd5f60b03d978c8f4e405
Alex <alexeytimonov1@gmail.com>
2024-12-28 18:14:21 +0200

    final code

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from curses.textpad import rectangle
+from threading import Thread
 
 import cv2
 import time
+from emailing import send_email
+import glob
+from threading import Thread
+import os
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
+
+def clean_images():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 while True:
+    status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -28,6 +42,27 @@ while True:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_thread = Thread(target=send_email, args=(image_with_object, ))
+        send_thread.daemon = True
+        clean_thread = Thread(target=clean_images)
+        clean_thread.daemon = True
+
+        send_thread.start()
+
 
     cv2.imshow("Video", frame)
     key=cv2.waitKey(1)
@@ -36,3 +71,4 @@ while True:
         break
 
 video.release()
+clean_thread.start()
